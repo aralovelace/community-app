a6b9fd74015db211e86ae2d2cd7e9f41e205ee74
April Smith <aprilroseannkwong@gmail.com>
2020-06-30 21:47:39 +0100

    Add more validation in the registration form
    - Add confirm password
    -  add extra validation

--- src/app/User/register/register.component.ts
+++ src/app/User/register/register.component.ts
@@ -1,47 +1,66 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { AuthService} from '../../Core/auth.service';
 import { Router, Params} from '@angular/router';
-import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {MustMatch} from '../../Helpers/must-match.validator';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
 
   registerForm: FormGroup;
   errorMessage = '';
   successMessage = '';
+  submitted = false;
 
   constructor(
     public authService: AuthService,
     private router: Router,
     private fb: FormBuilder
   ) {
-    this.createForm();
+
   }
 
-  createForm() {
+  ngOnInit() {
     this.registerForm = this.fb.group({
-      email: ['', Validators.required ],
-      password: ['', Validators.required]
+      email: ['', [Validators.required, Validators.email ]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: MustMatch('password', 'confirmPassword')
     });
   }
+
+  onSubmit(): void {
+
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    }
+    this.tryRegister(this.registerForm.value);
+  }
+
+  get er() { return this.registerForm.controls; }
+
 
 
   tryRegister(value){
 
     this.authService.doRegister(value)
       .then(res => {
-        console.log(res);
+
         this.errorMessage = '';
         this.successMessage = 'Your account has been created';
+        this.router.navigate(['/user']);
       }, err => {
         console.log(err);
         this.errorMessage = err.message;
         this.successMessage = '';
         });
+
   }
 
 }
